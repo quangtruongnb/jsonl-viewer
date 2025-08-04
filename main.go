1687a2c06ca79d88c5248e56883da37e3087ba93
Nguyễn Quang Trường <truongnq@workpc-2.local>
2025-08-04 08:18:16 +0700

    refactor: optimize style

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "jsonl-viewer",
+		Title:  "Jsonl Viewer",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
